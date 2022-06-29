a0b3c051daec446ff75ce9152a2296bc85c0dd72
1leha <88471482+1leha@users.noreply.github.com>
2022-06-29 17:37:10 +0000

    Deploying to gh-pages from @ 1leha/training-JS-9-nodejs-asinc-promice@e7e0b92126707f88ce84b12221acf9c5ec1f0341 🚀

---
+++ index.8ec77f2a.js
@@ -0,0 +1,2 @@
+console.log("1111"),console.log("1111");
+//# sourceMappingURL=index.8ec77f2a.js.map
---
+++ index.ae4a93ab.js
@@ -0,0 +1,2 @@
+console.log("1111"),console.log("1111");
+//# sourceMappingURL=index.ae4a93ab.js.map
